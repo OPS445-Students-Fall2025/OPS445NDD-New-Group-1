743f9e865b7051814954ae01c2bd98f3f7e06cae
Jaiveer Chawla <jchawla3@myseneca.ca>
2025-12-01 21:20:55 -0500

    Fully finished UserEnumerator.py script

--- UserEnumerator.py
+++ UserEnumerator.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+
+#UserEnumerator.py file
+
+#Part of Assignment 2 - Security Auditing
+
+#Author:        Jaiveer Chawla
+#Student ID:    101618205
+
 import subprocess, sys
 
 def run_a_linux_command(command):
@@ -71,7 +80,7 @@ if len(accounts_with_uid_0) > 1:
     other_account_that_has_uid_0_username = extract_account_username(other_account_that_has_uid_0)
 
     #Display recommendations.
-    print("\n" + "RECOMMENDATIONS: Immediately delete user account '" + other_account_that_has_uid_0_username + "'. Investigate system for possible cyber attack involving user account '" + other_account_that_has_uid_0_username + "'." + "\n")
+    print("\n" + "RECOMMENDATIONS: Immediately delete user account '" + other_account_that_has_uid_0_username + "'. Investigate system for possible cyber attack involving user account '" + other_account_that_has_uid_0_username + "'." + "\n\n")
 
 #Display 'GOOD' message and give no recommendations.
 else:
@@ -83,7 +92,7 @@ else:
         print("    - ",account)
     
     #Display recommendations.
-    print("\n" + "RECOMMENDATIONS: None" + "\n")
+    print("\n" + "RECOMMENDATIONS: None" + "\n\n")
 
 print("====================================================================" + "\n" + "#2: List Of Only User Accounts (Not System Accounts)" + "\n" + "====================================================================")
 account_usernames = [] #List that stores only the account username field from accounts extracted from /etc/passwd.
@@ -94,7 +103,7 @@ for account in only_user_accounts:
     account_usernames.append(account_username)
     print("    -" + account_username)
 
-print("")
+print("\n")
 
 account_with_longest_username = max(account_usernames, key=len) #This info is useful in properly formatting the output.
 
@@ -134,16 +143,25 @@ print("\n" + "User Accounts With Sudo Access:" + "\n" + "-----------------------
 for account in accounts_with_sudo_access:
         print(account)
 
-print("")
+print("\n")
 
-print("====================================================================" + "\n" + "#4: Last Logins For Each User. >= 2 Weeks Is Flagged" + "\n" + "====================================================================")
+print("====================================================================" + "\n" + "#4: Last Logins For Each User. 2 Weeks Or Older Is Flagged" + "\n" + "====================================================================")
 print("Last Login For User: " + "\n" + "--------------------")
+
+accounts_with_last_login_2_weeks_or_older = []
 
 #Go through every account and print out each account's last login
 for account in account_usernames:
 
-    last_login = str("last | grep " + account + " -m1")
-    last_login = (run_a_linux_command(last_login))
+    #Get last login for account by using the last command in Linux. The first result will be the most recent login, hence the pipe to grep with -m1 option.
+    #Must truncate account username if its greater than 8 chars in length. This is due to the last command only showcasing the first 8 chars of an account's username.
+    if len(account) > 8:
+        account_username_truncated = account[0:8]
+        last_login = str("last -F | grep " + account_username_truncated + " -m1")
+        last_login = (run_a_linux_command(last_login))
+    else:
+        last_login = str("last -F | grep " + account + " -m1")
+        last_login = (run_a_linux_command(last_login))
 
     #Convert to string so it can be split based on where the account login dates are.
     last_login = str(last_login)
@@ -162,7 +180,38 @@ for account in account_usernames:
     #This means that user had a recorded last login
     if len(last_login) > 0:
         print(account + spaces_to_add + "----->  " + last_login)
-    
+
+        #Check to see if last login is 2 weeks or older than current date. If so, add that account to the accounts_with_login_2_weeks_or_older list.
+        last_login = last_login.split(" ")
+        last_login = last_login[1:5] #Obtain only the login month, day, time, and year.
+        last_login = list(last_login) #Convert to list so that each element can be for looped to produce a new proper string.
+        last_login_str = "" #Hold the new proper string
+        for item in last_login:
+            last_login_str = last_login_str + item + " "
+
+        last_login_str = last_login_str.strip() #Remove extra whitespace at the end.
+
+        #Convert human-readable date to epoch seconds
+        convert_human_readable_date_to_epoch_seconds_command = str("date -d '" + last_login_str + "' " + "+%s")
+        account_last_login_epoch_seconds = run_a_linux_command(convert_human_readable_date_to_epoch_seconds_command)
+        account_last_login_epoch_seconds = int(account_last_login_epoch_seconds[0])
+        
+        #Determine if account's last login is 2 weeks or older. This is accomplished by subtracting the account's last login in epoch seconds from the current date epoch's seconds. If the result is equal to or greater than 2 weeks in epoch seconds which is 1,209,600, then that account's last login was 2 weeks or older. In that case, it will be added to the accounts_with_last_login_2_weeks_or_older list.
+        current_date_in_epoch_seconds_command = str("date +%s")
+        current_date_in_epoch_seconds = run_a_linux_command(current_date_in_epoch_seconds_command)
+        current_date_in_epoch_seconds = int(current_date_in_epoch_seconds[0])
+        
+        epoch_time_diff_between_current_date_and_user_acc_last_login = current_date_in_epoch_seconds - account_last_login_epoch_seconds
+
+        if epoch_time_diff_between_current_date_and_user_acc_last_login >= 1209600:
+            accounts_with_last_login_2_weeks_or_older.append(account)
+        else:
+            continue
+
     #Means user has never logged into the system
     else:
         print(account + spaces_to_add + "----->  " + "Never Logged Into The System")
+
+print("\n" + "Users Whos Login Is 2 Weeks Or Older:" + "\n" + "-------------------------------------")
+for account in accounts_with_last_login_2_weeks_or_older:
+    print(account)
